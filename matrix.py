4fab5ddaccd43d58e6f7d0525f6cadb28de0548d
0binna-oss <richardokeiyi@gmail.com>
2024-07-22 21:30:21 -0700

    matrix multiplication

---
+++ matrix.py
@@ -0,0 +1,16 @@
+import numpy as np 
+
+# input matrices as numpy arrays 
+matrix1 = np.array([[12, 7, 3],
+                   [4, 5, 6],
+                   [7, 8, 9]])
+
+matrix2 = np.array([[5, 8, 1],
+                    [6, 7, 3],
+                    [4, 5, 9]])
+
+# Perform matrix multiplication using @ operator 
+res = matrix1 @ matrix2 
+
+print("Result of matrix multiplication.")
+print(res) 
